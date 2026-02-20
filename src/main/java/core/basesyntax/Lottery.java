d932d301550a97d87042b05c8ec7ebaac0b44ce8
Oleksandr Fetisov <sasafetisov03@gmail.com>
2026-02-20 13:08:54 +0200

    feature: implemented hw lottery according to task requirements

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int MAX_BALL_NUMBER_VALUE = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+
+    public Ball getRandomBall() {
+        String color = colorSupplier.getRandomColor();
+        int number = random.nextInt(MAX_BALL_NUMBER_VALUE);
+        return new Ball(number, color);
+    }
+}
